609c7432b69000ddea587e031bf787de84847236
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-01-03 20:13:09 +0900

    Update FenwickTree.test.cpp

--- yosupo/FenwickTree.test.cpp
+++ yosupo/FenwickTree.test.cpp
@@ -7,7 +7,7 @@ using ll = long long;
 int main() {
   int n, q;
   std::cin >> n >> q;
-  kyopro::FenwickTree<ll> ft;
+  kyopro::FenwickTree<ll> ft(n);
   for (int i = 0; i < n; ++i) {
     int a;
     std::cin >> a;
