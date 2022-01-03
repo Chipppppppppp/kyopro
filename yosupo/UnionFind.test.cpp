b4d1f1a2cbc8ffffb03560de652fd79f8888be19
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-01-03 19:04:05 +0900

    Update and rename example.test.cpp to yosupo/UnionFind.test.cpp

---
+++ yosupo/UnionFind.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include <iostream>
+#include "../structure/UnionFind.hpp"
+
+int main() {
+  int n, q;
+  std::cin >> n >> q;
+  kyopro::UnionFind uf(n);
+  for (int i = 0; i < q; ++i) {
+    int t, u, v;
+    std::cin >> t >> u >> v;
+    if (t == 0) uf.unite(u, v);
+    else std::cout << uf.same(u, v) << '\n';
+  }
+}
