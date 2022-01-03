6586b5838c612cc3c7a338a80e9ed0a083f6ed02
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-01-03 21:46:17 +0900

    Update FenwickTree.hpp

--- structure/FenwickTree.hpp
+++ structure/FenwickTree.hpp
@@ -3,38 +3,51 @@
 /* FenwickTree */
 #include <cstdint>
 #include <vector>
+#include <utility>
 #include "../base/base_int.hpp"
 
 namespace kyopro {
-  template<class T, class Container = std::vector<T>>
+  template<class KyoproT, class KyoproContainer = std::vector<KyoproT>>
   struct FenwickTree {
   private:
-    Container kyopro_tree;
+    KyoproContainer kyopro_tree;
   public:
-    using value_type = T;
+    using value_type = KyoproT;
     using size_type = std::size_t;
-    using reference = T&;
-    using const_reference = const T&;
+    using reference = KyoproT&;
+    using const_reference = const KyoproT&;
     FenwickTree() noexcept = default;
     FenwickTree(std::size_t kyopro_n) noexcept: kyopro_tree(kyopro_n) {}
+    template<class... KyoproArgs>
+    FenwickTree(KyoproArgs&&... kyopro_args) noexcept {
+      KyoproContainer kyopro_a(forward<KyoproArgs>(kyopro_args)...);
+      kyopro_tree(kyopro_a.size());
+      int kyopro_cnt = 0;
+      for (KyoproT&& kyopro_i: kyopro_a) {
+        add(kyopro_cnt, kyopro_i);
+        ++kyopro_cnt;
+      }
+    }
     std::size_t size() noexcept { return kyopro_tree.size(); }
-    void add(int kyopro_p, const T& kyopro_x) {
+    void add(int kyopro_p, const KyoproT& kyopro_x) {
       ++kyopro_p;
       while (kyopro_p <= (int)size()) {
         kyopro_tree[kyopro_p - 1] += kyopro_x;
         kyopro_p += kyopro_p & -kyopro_p;
       }
     }
-    T query(int kyopro_r) {
-      T kyopro_s = 0;
+    void set(int kyopro_p, const KyoproT& kyopro_x) { add(kyopro_p, kyopro_x - get(kyopro_p)); }
+    KyoproT query(int kyopro_r) {
+      KyoproT kyopro_s = 0;
       while (kyopro_r > 0) {
         kyopro_s += kyopro_tree[kyopro_r - 1];
         kyopro_r -= kyopro_r & -kyopro_r;
       }
       return kyopro_s;
     }
-    T query(int kyopro_l, int kyopro_r) { return query(kyopro_r) - query(kyopro_l); }
-    T all_query() { return query(kyopro_tree.size()); }
+    KyoproT query(int kyopro_l, int kyopro_r) { return query(kyopro_r) - query(kyopro_l); }
+    KyoproT get(int kyopro_p) { return query(kyopro_p + 1) - query(kyopro_p); }
+    KyoproT all_query() { return query(kyopro_tree.size()); }
   };
 }
 #endif
