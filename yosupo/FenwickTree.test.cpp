d38204e21e9e8c8ce92577f0a6a252456f70c74d
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-01-03 19:54:45 +0900

    Update and rename FenwickTree.test.hpp to FenwickTree.test.cpp

---
+++ yosupo/FenwickTree.test.cpp
@@ -0,0 +1,22 @@
+#define problem "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include <iostream>
+#include <vector>
+#include "../structure/FenwickTree.hpp"
+using ll = long long;
+
+int main() {
+  int n, q;
+  std::cin >> n >> q;
+  kyopro::FenwickTree<ll> ft;
+  for (int i = 0; i < n; ++i) {
+    int a;
+    std::cin >> a;
+    ft.add(i, a);
+  }
+  for (int i = 0; i < q; ++i) {
+    int t, x, y;
+    std::cin >> t >> x >> y;
+    if (t == 0) ft.add(x, y);
+    else std::cout << ft.query(x, y) << '\n';
+  }
+}
