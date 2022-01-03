cc0092e63420820688c4fbdce724fabb7f4d3c8a
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-01-03 23:22:10 +0900

    Update and rename base_int.hpp to types.hpp
    added KYOPRO_BASE_UINT and KYOPRO_BASE_FLOAT

--- base/base_int.hpp
+++
@@ -1,4 +0,0 @@
-#include <cstdint>
-#ifndef KYOPRO_BASE_INT
-#define KYOPRO_BASE_INT std::int64_t
-#endif
---
+++ base/types.hpp
@@ -0,0 +1,10 @@
+#include <cstdint>
+#ifndef KYOPRO_BASE_INT
+#define KYOPRO_BASE_INT std::int64_t
+#endif
+#ifndef KYOPRO_BASE_UINT
+#define KYOPRO_BASE_UINT std::size_t
+#endif
+#ifndef KYOPRO_BASE_FLOAT
+#define KYOPRO_BASE_FLOAT double
+#endif
