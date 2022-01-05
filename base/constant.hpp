305bb1ae09f183f3c3f89b991254e915fb44b894
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-01-05 11:14:02 +0900

    added something

---
+++ base/constant.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <limits>
+#include <array>
+#include <utility>
+#include "settings.hpp"
+
+namespace kyopro {
+  template<class KyoproT>
+  inline constexpr KyoproT MOD = KYOPRO_DEFAULT_MOD;
+  inline constexpr KYOPRO_BASE_INT mod = MOD<KYOPRO_BASE_INT>;
+  template<class KyoproT>
+  inline constexpr KyoproT INF = std::numeric_limits<KyoproT>::max() / KYOPRO_INF_DIV;
+  inline constexpr KYOPRO_BASE_INT inf = INF<KYOPRO_BASE_INT>;
+  template<class KyoproT>
+  inline constexpr KyoproT PI = 3.14159265358979323846;
+  inline constexpr KYOPRO_BASE_FLOAT pi = PI<KYOPRO_BASE_FLOAT>;
+  inline constexpr std::array<std::pair<KYOPRO_BASE_INT, KYOPRO_BASE_INT>, 4> beside{{{1, 0}}, {{0, 1}}, {{-1, 0}}, {{0, -1}}};
+  inline constexpr std::array<std::pair<KYOPRO_BASE_INT, KYOPRO_BASE_INT>, 8> around{{{1, 0}}, {{1, 1}}, {{0, 1}}, {{-1, 1}}, {{-1, 0}}, {{-1, -1}}, {{0, -1}}, {{1, -1}}};
+}
--- structure/FenwickTree.hpp
+++ structure/FenwickTree.hpp
@@ -3,9 +3,10 @@
 #include <vector>
 #include <utility>
 #include "../base/settings.hpp"
+#include "../base/monoid.hpp"
 
 namespace kyopro {
-  template<class KyoproT, class KyoproContainer = std::vector<KyoproT>>
+  template<class KyoproT, class KyoproOp = Plus<KyoproT>, class KyoproContainer = std::vector<KyoproT>>
   struct FenwickTree {
   private:
     KyoproContainer kyopro_tree;
@@ -15,26 +16,26 @@ namespace kyopro {
     using reference = KyoproT&;
     using const_reference = const KyoproT&;
     FenwickTree() noexcept = default;
-    FenwickTree(KYOPRO_BASE_UINT kyopro_n) noexcept: kyopro_tree(kyopro_n) {}
+    FenwickTree(KYOPRO_BASE_UINT kyopro_n) noexcept: kyopro_tree(kyopro_n, kyopro_id) {}
     KYOPRO_BASE_UINT size() noexcept { return kyopro_tree.size(); }
-    void add(int kyopro_p, const KyoproT& kyopro_x) {
+    void apply(int kyopro_p, const KyoproT& kyopro_x) {
       ++kyopro_p;
       while (kyopro_p <= (int)size()) {
-        kyopro_tree[kyopro_p - 1] += kyopro_x;
+        kyopro_tree[kyopro_p - 1] = KyoproOp::op(kyopro_tree[kyopro_p - 1], kyopro_x);
         kyopro_p += kyopro_p & -kyopro_p;
       }
     }
-    void set(int kyopro_p, const KyoproT& kyopro_x) { add(kyopro_p, kyopro_x - get(kyopro_p)); }
-    KyoproT query(int kyopro_r) {
-      KyoproT kyopro_s = 0;
+    KyoproT prod(int kyopro_r) const {
+      KyoproT kyopro_s = KyoproOp::id;
       while (kyopro_r > 0) {
-        kyopro_s += kyopro_tree[kyopro_r - 1];
+        kyopro_s = KyoproOp::op(kyopro_s, kyopro_tree[kyopro_r - 1]);
         kyopro_r -= kyopro_r & -kyopro_r;
       }
       return kyopro_s;
     }
-    KyoproT query(int kyopro_l, int kyopro_r) { return query(kyopro_r) - query(kyopro_l); }
-    KyoproT get(int kyopro_p) { return query(kyopro_p + 1) - query(kyopro_p); }
-    KyoproT all_query() { return query(kyopro_tree.size()); }
+    KyoproT prod(int kyopro_l, int kyopro_r) const { return KyoproOp::op(prod(kyopro_r), KyoproOp::inv(prod(kyopro_l))); }
+    KyoproT get(int kyopro_p) { return KyoproOp::op(prod(kyopro_p + 1), KyoproOp::inv(prod(kyopro_p))); }
+    KyoproT all_query() { return prod(kyopro_tree.size()); }
+    void set(int kyopro_p, const KyoproT& kyopro_x) { apply(kyopro_p, KyoproOp::op(kyopro_x, KyoproOp::inv(get(kyopro_p)))); }
   };
 }
