f54a9b9395ba0c7f61876dfa4d60b35b3e41bc75
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-03-07 19:01:57 +0900

    changed something

--- math/monoid.hpp
+++ math/monoid.hpp
@@ -5,27 +5,31 @@
 namespace kyopro {
   template<class _typeT, _typeT _id = 0>
   struct Plus {
+    static_assert(std::is_arithmetic_v<_typeT>);
     static constexpr _typeT id = _id;
-    static constexpr _typeT op(_typeT _a, _typeT _b) noexcept { return _a + _b; }
-    static constexpr _typeT inv(_typeT _a) noexcept { return -_a; }
+    constexpr _typeT operator ()(_typeT _a, _typeT _b) const noexcept { return _a + _b; }
+    constexpr _typeT inv(_typeT _a) const noexcept { return -_a; }
   };
   template<class _typeT, _typeT _id = 1>
   struct Mul {
+    static_assert(std::is_arithmetic_v<_typeT>);
     static constexpr _typeT id = _id;
-    static constexpr _typeT op(_typeT _a, _typeT _b) noexcept { return _a * _b; }
-    static constexpr _typeT inv(_typeT _a) noexcept {
+    constexpr _typeT operator ()(_typeT _a, _typeT _b) const noexcept { return _a * _b; }
+    constexpr _typeT inv(_typeT _a) const noexcept {
       static_assert(!std::is_integral_v<_typeT>);
       return 1 / _a;
     }
   };
-  template<class _typeT, _typeT _id = -inf>
+  template<class _typeT, _typeT _id = std::is_integral_v<_typeT> ? -INF<_typeT> : -inf>
   struct Max {
+    static_assert(std::is_arithmetic_v<_typeT>);
     static constexpr _typeT id = _id;
-    static constexpr _typeT op(_typeT _a, _typeT _b) noexcept { return _a > _b ? _a : _b; }
+    constexpr _typeT operator ()(_typeT _a, _typeT _b) const noexcept { return _a > _b ? _a : _b; }
   };
-  template<class _typeT, _typeT _id = inf>
+  template<class _typeT, _typeT _id = std::is_integral_v<_typeT> ? INF<_typeT> : inf>
   struct Min {
+    static_assert(std::is_arithmetic_v<_typeT>);
     static constexpr _typeT id = _id;
-    static constexpr _typeT op(_typeT _a, _typeT _b) noexcept { return _a < _b ? _a : _b; }
+    constexpr _typeT operator ()(_typeT _a, _typeT _b) const noexcept { return _a < _b ? _a : _b; }
   };
 }
--- structure/FenwickTree.hpp
+++ structure/FenwickTree.hpp
@@ -9,6 +9,7 @@ namespace kyopro {
   template<class _typeT, class _typeOp = Plus<_typeT>>
   struct FenwickTree {
   private:
+    [[no_unique_address]] _typeOp _op;
     std::vector<_typeT> _tree;
 
   public:
@@ -18,32 +19,32 @@ namespace kyopro {
     using const_reference = const _typeT&;
 
     FenwickTree() noexcept = default;
-    FenwickTree(KYOPRO_BASE_UINT _n) noexcept: _tree(_n, _typeOp::id) {}
+    FenwickTree(KYOPRO_BASE_UINT _n) noexcept: _op(), _tree(_n, _op.id) {}
 
     KYOPRO_BASE_UINT size() noexcept { return _tree.size(); }
 
     void apply(int _p, const _typeT& _x) {
       ++_p;
       while (_p <= (int)size()) {
-        _tree[_p - 1] = _typeOp::op(_tree[_p - 1], _x);
+        _tree[_p - 1] = _op(_tree[_p - 1], _x);
         _p += _p & -_p;
       }
     }
 
     _typeT prod(int _r) const {
-      _typeT _s = _typeOp::id;
+      _typeT _s = _op.id;
       while (_r > 0) {
-        _s = _typeOp::op(_s, _tree[_r - 1]);
+        _s = _op(_s, _tree[_r - 1]);
         _r -= _r & -_r;
       }
       return _s;
     }
-    _typeT prod(int _l, int _r) const { return _typeOp::op(prod(_r), _typeOp::inv(prod(_l))); }
+    _typeT prod(int _l, int _r) const { return _op(prod(_r), _op.inv(prod(_l))); }
 
     _typeT all_prod() { return prod(_tree.size()); }
 
-    _typeT get(int _p) { return _typeOp::op(prod(_p + 1), _typeOp::inv(prod(_p))); }
+    _typeT get(int _p) { return _op(prod(_p + 1), _op.inv(prod(_p))); }
 
-    void set(int _p, const _typeT& _x) { apply(_p, _typeOp::op(_x, _typeOp::inv(get(_p)))); }
+    void set(int _p, const _typeT& _x) { apply(_p, _op(_x, _op.inv(get(_p)))); }
   };
 }
