86d486e2c41b12c9248d4a0d5b41505f532f56f5
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-03-09 23:09:50 +0900

    use kyopro::scan instead of std::cin

--- yosupo/FenwickTree.test.cpp
+++ yosupo/FenwickTree.test.cpp
@@ -1,20 +1,20 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
 #include <iostream>
 #include "../structure/FenwickTree.hpp"
-#include "../base/out.hpp"
+#include "../base/io.hpp"
 
 int main() {
   int n, q;
-  std::cin >> n >> q;
+  kyopro::scan(n, q);
   kyopro::FenwickTree<long long> ft(n);
   for (int i = 0; i < n; ++i) {
     int a;
-    std::cin >> a;
+    kyopro::scan(a);
     ft.apply(i, a);
   }
   for (int i = 0; i < q; ++i) {
     int t, x, y;
-    std::cin >> t >> x >> y;
+    kyopro::scan(t, x, y);
     if (t == 0) ft.apply(x, y);
     else kyopro::println(ft.prod(x, y));
   }
--- yosupo/UnionFind.test.cpp
+++ yosupo/UnionFind.test.cpp
@@ -1,15 +1,15 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 #include <iostream>
 #include "../structure/UnionFind.hpp"
-#include "../base/out.hpp"
+#include "../base/io.hpp"
 
 int main() {
   int n, q;
-  std::cin >> n >> q;
+  kyopro::scan(n, q);
   kyopro::UnionFind uf(n);
   for (int i = 0; i < q; ++i) {
     int t, u, v;
-    std::cin >> t >> u >> v;
+    kyopro::scan(t, u, v);
     if (t == 0) uf.merge(u, v);
     else kyopro::println(uf.same(u, v));
   }
