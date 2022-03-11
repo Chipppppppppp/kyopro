42aacac9a2ed33db2fe6cb7405b56b0acd88dcff
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-03-12 00:02:47 +0900

    deleted constexpr

--- math/DynamicModInt.hpp
+++ math/DynamicModInt.hpp
@@ -12,24 +12,24 @@ namespace kyopro {
     inline static KYOPRO_BASE_UINT mod;
     KYOPRO_BASE_UINT value;
 
-    static constexpr void set_mod(KYOPRO_BASE_UINT _mod) noexcept {
+    static void set_mod(KYOPRO_BASE_UINT _mod) noexcept {
       mod = _mod;
     }
 
-    constexpr DynamicModInt() noexcept = default;
+    DynamicModInt() noexcept = default;
     template<class _typeT>
-    constexpr DynamicModInt(_typeT _value) noexcept: value(floor_mod(_value, mod)) { static_assert(std::is_integral_v<_typeT>); }
+    DynamicModInt(_typeT _value) noexcept: value(floor_mod(_value, mod)) { static_assert(std::is_integral_v<_typeT>); }
 
     template<class _typeT>
-    explicit constexpr operator _typeT() const noexcept { return value; }
+    explicit operator _typeT() const noexcept { return value; }
 
-    static constexpr DynamicModInt raw(std::uint_fast64_t _n) noexcept {
+    static DynamicModInt raw(std::uint_fast64_t _n) noexcept {
       DynamicModInt _res;
       _res.value = _n;
       return _res;
     }
 
-    constexpr DynamicModInt power(std::uint_fast64_t _n) const noexcept {
+    DynamicModInt power(std::uint_fast64_t _n) const noexcept {
       std::uint_fast64_t _res = 1, _a = value;
       while (_n > 0) {
         if (_n & 1) _res = _res * _a % mod;
@@ -39,7 +39,7 @@ namespace kyopro {
       return _res;
     }
 
-    constexpr DynamicModInt inv() const noexcept {
+    DynamicModInt inv() const noexcept {
       std::uint_fast64_t _a = value, _b = mod;
       std::int_fast64_t _u = 1, _v = 0;
       while (_b > 0) {
@@ -52,65 +52,65 @@ namespace kyopro {
       return floor_mod(_u, mod);
     }
 
-    constexpr DynamicModInt operator +() const noexcept { return *this; }
+    DynamicModInt operator +() const noexcept { return *this; }
 
-    constexpr DynamicModInt operator -() const noexcept { return mod - value; }
+    DynamicModInt operator -() const noexcept { return mod - value; }
 
-    constexpr DynamicModInt& operator ++() noexcept {
+    DynamicModInt& operator ++() noexcept {
       if (++value >= mod) value -= mod;
       return *this;
     }
 
-    constexpr DynamicModInt operator ++(int) noexcept {
+    DynamicModInt operator ++(int) noexcept {
       DynamicModInt _before = *this;
       operator ++();
       return _before;
     }
 
-    constexpr DynamicModInt& operator --() noexcept {
+    DynamicModInt& operator --() noexcept {
       if (value == 0) value = mod;
       --value;
       return *this;
     }
 
-    constexpr DynamicModInt operator --(int) noexcept {
+    DynamicModInt operator --(int) noexcept {
       DynamicModInt _before = *this;
       operator --();
       return _before;
     }
 
-    constexpr DynamicModInt& operator +=(DynamicModInt _rhs) noexcept {
+    DynamicModInt& operator +=(DynamicModInt _rhs) noexcept {
       if ((value += _rhs.value) >= mod) value -= mod;
       return *this;
     }
 
-    constexpr DynamicModInt& operator -=(DynamicModInt _rhs) noexcept {
+    DynamicModInt& operator -=(DynamicModInt _rhs) noexcept {
       if (value < _rhs.value) value += mod;
       value -= _rhs.value;
       return *this;
     }
 
-    constexpr DynamicModInt& operator *=(DynamicModInt _rhs) noexcept {
+    DynamicModInt& operator *=(DynamicModInt _rhs) noexcept {
       value = value * _rhs.value % mod;
       return *this;
     }
 
-    constexpr DynamicModInt& operator /=(DynamicModInt _rhs) noexcept {
+    DynamicModInt& operator /=(DynamicModInt _rhs) noexcept {
       value = value * _rhs.inv().value % mod;
       return *this;
     }
 
-    friend constexpr DynamicModInt operator +(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs += _rhs; }
+    friend DynamicModInt operator +(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs += _rhs; }
 
-    friend constexpr DynamicModInt operator -(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs -= _rhs; }
+    friend DynamicModInt operator -(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs -= _rhs; }
 
-    friend constexpr DynamicModInt operator *(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs *= _rhs; }
+    friend DynamicModInt operator *(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs *= _rhs; }
 
-    friend constexpr DynamicModInt operator /(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs /= _rhs; }
+    friend DynamicModInt operator /(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs /= _rhs; }
 
-    friend constexpr bool operator ==(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs.value == _rhs.value; }
+    friend bool operator ==(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs.value == _rhs.value; }
 
-    friend constexpr bool operator !=(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs.value != _rhs.value; }
+    friend bool operator !=(DynamicModInt _lhs, DynamicModInt _rhs) noexcept { return _lhs.value != _rhs.value; }
 
     template<class _typeScanner>
     void scan(_typeScanner& _scanner) {
@@ -125,5 +125,5 @@ namespace kyopro {
     }
   };
 
-  struct Hash<DynamicModInt> { constexpr std::size_t operator ()(DynamicModInt _a) const noexcept { return _a; } };
+  struct Hash<DynamicModInt> { std::size_t operator ()(DynamicModInt _a) const noexcept { return _a; } };
 }
