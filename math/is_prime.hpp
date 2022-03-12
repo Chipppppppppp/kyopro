6e826171e6fe16fd81afd6727484742a86e2103c
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-03-12 19:21:33 +0900

    fixed

--- math/is_prime.hpp
+++ math/is_prime.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <array>
 #include <cstdint>
+#include <initializer_list>
 #include "../base/settings.hpp"
 #include "modpow.hpp"
 
@@ -11,7 +11,7 @@ namespace kyopro {
     std::uint_fast64_t _d = _n - 1;
     while (!(_d & 1)) _d >>= 1;
     std::uint_fast64_t _e = 1, _rev = _n - 1;
-    for (auto _a: std::array<std::uint_fast64_t, 7>{2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+    for (auto _a: (_n < (1ULL << 32) ? std::initializer_list<std::uint_fast64_t>{2, 7, 61} : std::initializer_list<std::uint_fast64_t>{2, 325, 9375, 28178, 450775, 9780504, 1795265022})) {
       if (_n <= _a) break;
       std::uint_fast64_t _t = _d, _y = modpow(_a, _d, _n);
       while (_t != _n - 1 && _y != _e && _y != _rev) {
