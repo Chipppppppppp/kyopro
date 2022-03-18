1187f50c33b3e7a14912f4d778cbc3411c22e237
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-03-19 00:55:42 +0900

    fixed

--- all/all.hpp
+++ all/all.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include "algorithm/all.hpp"
-#include "math/all.hpp"
-#include "meta/all.hpp"
-#include "structure/all.hpp"
-#include "system/all.hpp"
-#include "template/all.hpp"
+#include "../algorithm/all.hpp"
+#include "../math/all.hpp"
+#include "../meta/all.hpp"
+#include "../structure/all.hpp"
+#include "../system/all.hpp"
+#include "../template/all.hpp"
