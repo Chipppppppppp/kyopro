8503daa1a613aed2249b1bb797e7520661adc9ea
Chippppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-03-19 12:10:09 +0900

    Update mod.hpp

--- math/mod.hpp
+++ math/mod.hpp
@@ -5,7 +5,7 @@
 namespace kyopro {
   template<class _typeT, class _typeU>
   constexpr std::common_type_t<_typeT, _typeU> floor_mod(_typeT _x, _typeU _m) noexcept {
-    static_assert(std::is_integral_v<_typeT> && std::is_integral_v<_typeU>);
+    static_assert(std::is_integral_v<_typeT> && std::is_integral_v<_typeU>, "Integer is required");
     if constexpr (std::is_unsigned_v<_typeT> || std::is_unsigned_v<_typeU>) return _x % _m;
     return (_x %= _m) < 0 ? _x + _m : _x;
   }
