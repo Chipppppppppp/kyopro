c0f6987a13ed3cabac66da4e41667cc579d64e87
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-04-17 23:42:01 +0900

    fixed

--- algorithm/compress.hpp
+++ algorithm/compress.hpp
@@ -9,7 +9,7 @@
 namespace kyopro {
   template<class _typeT, class _typeContainer = std::unordered_map<typename std::iterator_traits<_typeT>::value_type, KYOPRO_BASE_INT>, class _typeCompare>
   auto compress(_typeT _first, _typeT _last, _typeCompare _comp = std::less<typename std::iterator_traits<_typeT>::value_type>()) {
-    std::vector<typename iterator_traits<_typeT>::value_type> _vec(_first, _last);
+    std::vector<typename std::iterator_traits<_typeT>::value_type> _vec(_first, _last);
     std::sort(_vec.begin(), _vec.end(), _comp);
     auto _end = std::unique(_vec.begin(), _vec.end());
     _typeContainer _mem;
