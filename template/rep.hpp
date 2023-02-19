1444dd723868e638d765b63dacc2aa1a271671a0
root <root@Chippppp>
2023-02-20 00:38:21 +0900

    Fix

--- template/rep.hpp
+++ template/rep.hpp
@@ -7,4 +7,4 @@
 #define KYOPRO_REP3(i, first, last) for (auto i = (first), KYOPRO_LAST = last; (i) < (KYOPRO_LAST); ++(i))
 
 #define KYOPRO_OVERLOAD_REP(_1, _2, _3, name, ...) name
-#define rep(...) KYOPRO_OVERLOAD_MACRO(__VA_ARGS__ __VA_OPT__(,) KYOPRO_REP3, KYOPRO_REP2, KYOPRO_REP1, KYOPRO_REP0)(__VA_ARGS__)
+#define rep(...) KYOPRO_OVERLOAD_REP(__VA_ARGS__ __VA_OPT__(,) KYOPRO_REP3, KYOPRO_REP2, KYOPRO_REP1, KYOPRO_REP0)(__VA_ARGS__)
