c04dfc51c49e6abe7809298d408fe971cf38ab12
root <root@Chippppp>
2023-04-23 01:38:27 +0900

    Fix

--- kpr/template/lambda_macro.hpp
+++ kpr/template/lambda_macro.hpp
@@ -1,20 +1,8 @@
 #pragma once
-#include <iterator>
+#include <cstddef>
 #include <tuple>
-#include "../meta/tuple_like.hpp"
 
 #define $(...) \
 ([&](auto&&... _args) { \
-    auto _args_tuple = std::forward_as_tuple(_args...); \
-    if constexpr (sizeof...(_args) == 0) { \
-        return ([&]() { return (__VA_ARGS__); })(); \
-    } else if constexpr (sizeof...(_args) == 1) { \
-        return ([&](auto&& $0) { return (__VA_ARGS__); })(get<0>(_args_tuple)); \
-    } else if constexpr (sizeof...(_args) == 2) { \
-        return ([&](auto&& $0, auto&& $1) { return (__VA_ARGS__); })(get<0>(_args_tuple), get<1>(_args_tuple)); \
-    } else if constexpr (sizeof...(_args) == 3) { \
-        return ([&](auto&& $0, auto&& $1, auto&& $2) { return (__VA_ARGS__); })(get<0>(_args_tuple), get<1>(_args_tuple), get<2>(_args_tuple)); \
-    } else if constexpr (sizeof...(_args) == 4) { \
-        return ([&](auto&& $0, auto&& $1, auto&& $2, auto&& $3) { return (__VA_ARGS__); })(get<0>(_args_tuple), get<1>(_args_tuple), get<2>(_args_tuple), get<3>(_args_tuple)); \
-    } \
+    return ([&]([[maybe_unused]] auto&& $0, [[maybe_unused]] auto&& $1, [[maybe_unused]] auto&& $2, [[maybe_unused]] auto&& $3, [[maybe_unused]] auto&& $4, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr) noexcept { return (__VA_ARGS__); })(std::forward<decltype(_args)>(_args)..., nullptr, nullptr, nullptr, nullptr, nullptr); \
 })
