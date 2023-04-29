9540a1d43b3d5ab2c0164bd7892f0138c8f07c35
root <root@Chippppp>
2023-04-29 19:36:33 +0900

    Fix

--- kpr/template/lambda_macro.hpp
+++ kpr/template/lambda_macro.hpp
@@ -1,8 +1,7 @@
 #pragma once
-#include <cstddef>
-#include <tuple>
+#include <utility>
 
 #define $(...) \
-([&](auto&&... _args) { \
-    return ([&]([[maybe_unused]] auto&& $0, [[maybe_unused]] auto&& $1, [[maybe_unused]] auto&& $2, [[maybe_unused]] auto&& $3, [[maybe_unused]] auto&& $4, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr) noexcept { return (__VA_ARGS__); })(std::forward<decltype(_args)>(_args)..., nullptr, nullptr, nullptr, nullptr, nullptr); \
+([&](auto&&... _lambda_macro_args) { \
+    return ([&]([[maybe_unused]] auto&& $0, [[maybe_unused]] auto&& $1, [[maybe_unused]] auto&& $2, [[maybe_unused]] auto&& $3, [[maybe_unused]] auto&& $4, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr, std::nullptr_t = nullptr) noexcept { return (__VA_ARGS__); })(std::forward<decltype(_lambda_macro_args)>(_lambda_macro_args)..., nullptr, nullptr, nullptr, nullptr, nullptr); \
 })
