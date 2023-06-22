439ec18af4338d58b29d813c6849e433a01e58a1
root <root@ChipppppLaptop.localdomain>
2023-06-22 12:22:29 +0900

    Fix

--- kpr/algorithm/count_all.hpp
+++ kpr/algorithm/count_all.hpp
@@ -1,12 +1,13 @@
 #pragma once
 #include <iterator>
 #include <unordered_map>
+#include "Hash.hpp"
 #include "../meta/setting.hpp"
 
 namespace kpr {
     // 要素: 個数の辞書を返す
     [[maybe_unused]] inline constexpr struct {
-        template<class T, class Container = std::unordered_map<typename std::iterator_traits<T>::value_type, KYOPRO_BASE_INT>>
+        template<class T, class Container = std::unordered_map<typename std::iterator_traits<T>::value_type, KYOPRO_BASE_INT, Hash<typename std::iterator_traits<T>::value_type>>>
         auto operator ()(T first, T last) const {
             Container mem;
             for (auto i = first; i != last; ++i) ++mem[*i];
