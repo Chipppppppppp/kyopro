3e86913cce95bdec2fe62ebd799edb1dff3b06e0
Chippppp <Chippppppppppppppp@gmail.com>
2024-03-27 17:18:34 +0900

    Fix

--- kpr/math/divisors.hpp
+++ kpr/math/divisors.hpp
@@ -14,7 +14,7 @@ namespace kpr {
                 upper.emplace_back(n / i);
             }
             if (i * i == n) lower.emplace_back(i);
-            lower.insert(end(lower), upper.rbegin(), upper.rend());
+            lower.insert(lower.end(), upper.rbegin(), upper.rend());
             return lower;
         }
     } divisors;
